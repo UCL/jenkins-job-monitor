7bef43d49e263f9b2c2e915e9a317432f9845e9c
James Hetherington <j.hetherington@ucl.ac.uk>
2013-09-04 16:34:53 +0100

    Add python scaffold

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='jenkinsjobmonitor',
+      version='0.01',
+      description='Monitor github for jenkins job info',
+      author='James Hetherington',
+      author_email='jamespjh@gmail.com',
+      packages=['jenkinsjobmonitor']
+)
